06cd7b50206af5f50afa3691608e70f9a0cea165
Rodrigo <dgrodrigo@gmail.com>
2013-03-23 12:33:36 -0300

    af2

--- Maroto.java
+++ Maroto.java
@@ -1,7 +1,7 @@
 class Maroto {
 
 	public static void main(String... args) {
-		System.out.println("Maceta 2 master");
+		System.out.println("Maceta");
 		System.out.println("Adicionado Maroto ");
 		System.out.println("Add Master"); 
 		System.out.println("Teste"); 
